5ecd8b35897fb6d2ca9c955bbb799ca3a5b147db
Paula Farell <pfarell1241@gmail.com>
2024-05-13 14:45:19 +0200

    Prueba

---
+++ GAME/js/main.ts
@@ -0,0 +1,24 @@
+import { LoadScene } from "./Escenes/Load";
+import { MenuScene } from "./Escenes/Menu";
+import { PlayScene } from "./Escenes/Nivell1";
+
+let game = new Phaser.Game({
+    width: 800,
+    height: 600,
+    scene: [
+        LoadScene, MenuScene, PlayScene
+    ],
+    render: {
+        pixelArt: true
+    },
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+});
